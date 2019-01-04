8507a6fcb4af1ac3f75941c8e9912784aa7af8c5
sperkn <nicholas.kreps@gmail.com>
2019-01-04 17:47:53 -0500

    functionality for manual adjusting progress bar/current time of video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,8 @@ const video = document.querySelector('.player__video');
 const playButton = document.querySelector('.player__button');
 const volumeSlider = document.querySelector('.player__controls input[name=volume]');
 const speedSlider = document.querySelector('.player__controls input[name=playbackRate]');
+const progress = document.querySelector('.player__controls .progress');
+const progressFilled = document.querySelector('.player__controls .progress__filled');
 
 function togglePlay() {
   if(video.paused) {
@@ -19,6 +21,13 @@ function adjustSpeed() {
   video.playbackRate = this.value;
 };
 
+function adjustProgress(e) {
+  let width = (e.layerX/640)*100;
+  progressFilled.style.flexBasis = `${width}%`;
+  video.currentTime = video.duration*(width/100);
+};
+
+progress.addEventListener('click', adjustProgress);
 playButton.addEventListener('click', togglePlay);
 volumeSlider.addEventListener('change', adjustVolume);
 speedSlider.addEventListener('change', adjustSpeed);
