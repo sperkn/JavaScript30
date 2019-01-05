f5108fe7ec7dcd0ff006836e69a5c3731debb734
sperkn <nicholas.kreps@gmail.com>
2019-01-05 16:41:42 -0500

    functionality for dragging progress on mousedown & full screen mode

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,8 +3,10 @@ const video = player.querySelector('.player__video');
 const playButton = player.querySelector('.player__button');
 const sliders = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
+const fullButton = player.querySelector('.player__full');
 const progress = player.querySelector('.player__controls .progress');
-const progressFilled = player.querySelector('.player__controls .progress__filled');
+const progressBar = player.querySelector('.player__controls .progress__filled');
+let mousedown = false;
 
 function togglePlay() {
   if(video.paused) {
@@ -27,17 +29,36 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 };
 
+function handleProgress() {
+  const percent = (video.currentTime/video.duration)*100;
+  progressBar.style.flexBasis = `${percent}%`;
+};
+
 function adjustProgress(e) {
-  let width = (e.layerX/640)*100;
-  progressFilled.style.flexBasis = `${width}%`;
-  video.currentTime = video.duration*(width/100);
+  const time = (e.offsetX/progress.offsetWidth) * video.duration;
+  video.currentTime = time;
+};
+
+function fullScreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+    fullButton.textContent = 'Full';
+  } else {
+    player.requestFullscreen();
+    fullButton.textContent = 'Esc';
+  }
 };
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 playButton.addEventListener('click', togglePlay);
 sliders.forEach(slider => slider.addEventListener('change', adjustSlider));
 sliders.forEach(slider => slider.addEventListener('mousemove', adjustSlider));
 skipButtons.forEach(button => button.addEventListener('click', skip));
 progress.addEventListener('click', adjustProgress);
+progress.addEventListener('mousemove', (e) => mousedown && adjustProgress(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+fullButton.addEventListener('click', fullScreen);
