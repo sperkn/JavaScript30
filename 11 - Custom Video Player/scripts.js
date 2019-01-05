9f736a37c8cb3281dabb193ab85269d13b24435c
sperkn <nicholas.kreps@gmail.com>
2019-01-05 11:41:55 -0500

    functionality for updating button from play/pause, skipping forward and back & refactored slider adjusting code

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,7 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.player__video');
 const playButton = player.querySelector('.player__button');
 const sliders = player.querySelectorAll('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const progress = player.querySelector('.player__controls .progress');
 const progressFilled = player.querySelector('.player__controls .progress__filled');
 
@@ -13,15 +14,18 @@ function togglePlay() {
   }
 };
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  playButton.textContent = icon;
+};
+
 function adjustSlider() {
-  const inputName = this.name;
-  
-  if (inputName === 'volume') {
-    video.volume = this.value;
-  } else {
-    video.playbackRate = this.value;
-  }
-}
+  video[this.name] = this.value;
+};
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+};
 
 function adjustProgress(e) {
   let width = (e.layerX/640)*100;
@@ -29,6 +33,11 @@ function adjustProgress(e) {
   video.currentTime = video.duration*(width/100);
 };
 
-progress.addEventListener('click', adjustProgress);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 playButton.addEventListener('click', togglePlay);
 sliders.forEach(slider => slider.addEventListener('change', adjustSlider));
+sliders.forEach(slider => slider.addEventListener('mousemove', adjustSlider));
+skipButtons.forEach(button => button.addEventListener('click', skip));
+progress.addEventListener('click', adjustProgress);
